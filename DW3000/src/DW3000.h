9460f77b473da1c034793db1635b41e7f1145416
Philipp Hafkemeyer <30576065+IxonTV@users.noreply.github.com>
2023-03-07 18:04:02 +0100

    Add files via upload
    added...
    examples folder
    src folder with DW3000.h and DW3000.cpp in it
    keywords.txt and updated its contents
    library.json
    library.properties
    license.md
    
    changed...
    all methods to static, so it can be reached more easily

---
+++ DW3000/src/DW3000.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023 by Philipp Hafkemeyer
+ * Qorvo DW3000 library for Arduino
+ * 
+ * This project is licensed under the GNU GPL v3.0 License.
+ * you may not use this file except in compliance with the License.
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+#ifndef Morse_h
+#define Morse_h
+
+#include "Arduino.h"
+class DW3000Class {
+	public:
+		static int getAnchorID();
+		static uint32_t read(int base, int sub);
+		static uint32_t write(int base, int sub, int *data, int data_len);
+		static void init();
+		static void readInit();
+		static void setLED1(uint8_t status);
+		static void setLED2(uint8_t status);
+	private:
+		static int* getBase(int hex_num);
+		static int* getSub(int hex_num);
+		static void writeShortCommand(int cmd[], int cmd_len);
+		static uint32_t readOrWriteFullAddress(int base[], int base_len, int sub[], int sub_len, int data[], int data_len, int readWriteBit);
+		static uint32_t readOrWriteFullAddress(int base[], int base_len, int sub[], int sub_len, int data[], int data_len, int readWriteBit, bool quiet);
+		static uint32_t sendBytes(int b[], int lenB, int recLen);
+		static int* hexToBin(int hex_num, int bit_size);
+		static bool is_anchor;
+		static int anchor_id;
+		static void resetIRQStatusBits();
+		static void interruptDetect();
+		static int getIRQBit();
+		static int readRXBuffer();
+};
+extern DW3000Class DW3000;
+#endif
